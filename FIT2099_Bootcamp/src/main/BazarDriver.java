9149b64adf8a7e1c72edeefae5ed2356c116559b
YewYeePerng <yeeperngyew@gmail.com>
2023-04-07 05:28:56 +0800

    Minor changes to accomodate with outputting

--- FIT2099_Bootcamp/src/main/BazarDriver.java
+++ FIT2099_Bootcamp/src/main/BazarDriver.java
@@ -2,14 +2,31 @@ package main;
 
 import main.controllers.PurchaseManager;
 import main.controllers.Store;
-import main.models.Purchase;
-import main.utils.MenuInput;
+import main.utils.MenuManagerAdmin;
+import main.utils.MenuManagerEmployee;
+
+import java.util.Scanner;
+
+/**
+ * @author Yee Perng Yew
+ * @version 1
+ * @see PurchaseManager, Store, MenuManagerAdmin, MenuManagerEmployee
+ */
 
 public class BazarDriver {
-    public static void main(String args[]) {
-        PurchaseManager purchaseManager = new PurchaseManager();
-        MenuInput menuInput = new MenuInput();
-        Store store = new Store(purchaseManager, menuInput);
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String args[]) throws Exception {
+//        PurchaseManager purchaseManager = new PurchaseManager();
+        PurchaseManager purchaseManager = PurchaseManager.getInstance();
+//        MenuManagerAdmin menuManagerAdmin = new MenuManagerAdmin();
+        MenuManagerAdmin menuManagerAdmin = MenuManagerAdmin.getInstance();
+        MenuManagerEmployee menuManagerEmployee = MenuManagerEmployee.getInstance();
+//        Store store = new Store(purchaseManager, menuManagerAdmin);
+        Store store = Store.getInstance(purchaseManager, menuManagerAdmin);
         store.runBazar();
 
 
