38a59ab7958c86284a9cfb8e021fdfb94bc301ef
YewYeePerng <yeeperngyew@gmail.com>
2023-04-07 05:29:20 +0800

    Java DocString added to all Classes

--- FIT2099_Bootcamp/src/main/controllers/IData.java
+++ FIT2099_Bootcamp/src/main/controllers/IData.java
@@ -1,6 +1,17 @@
 package main.controllers;
 
+/**
+ * @author Yee Perng Yew
+ * @version 1
+ * @see
+ */
+
 public interface IData {
+    /**
+     *
+     * @param id
+     * @return
+     */
     boolean isDeviceAvailable(int id);
 
 }
