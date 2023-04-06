0c7e797195873fd2fe6629806cfc741568976dbb
YewYeePerng <yeeperngyew@gmail.com>
2023-04-07 05:28:21 +0800

    Implementaion of IMenuManager

---
+++ FIT2099_Bootcamp/src/main/utils/IMenuManager.java
@@ -0,0 +1,13 @@
+package main.utils;
+/**
+ * @author Yee Perng Yew
+ * @version 1
+ * @see
+ */
+public interface IMenuManager {
+    /**
+     *
+     * @return
+     */
+    public int menuItem();
+}
